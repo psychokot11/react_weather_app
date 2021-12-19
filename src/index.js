053a918de51ca314ec96b353ba693199fc875218
psychokot11 <z.stawarska@gmail.com>
2021-12-19 20:50:03 +0100

    Installed bootstrap via terminal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      {" "}
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
