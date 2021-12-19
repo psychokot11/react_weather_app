1bbb1a403ae01735d994a7ac7279182a066926ab
psychokot11 <z.stawarska@gmail.com>
2021-12-19 16:50:43 +0100

    Styled footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,20 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
-  return "Hello from Footer";
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/psychokot11/SheCodesPlus_final"
+          class="openSourceFooterLink"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code{" "}
+        </a>
+        by Zosia S
+      </footer>
+    </div>
+  );
 }
