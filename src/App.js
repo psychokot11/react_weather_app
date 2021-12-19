b8e5d75e6042a756350548b74d4de4cb7c3f8fa6
psychokot11 <z.stawarska@gmail.com>
2021-12-19 16:18:12 +0100

    Added SearchEngine and Footer components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import SearchEngine from "./SearchEngine";
+import Footer from "./Footer";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="App-body">
+        <SearchEngine />
+        <hr />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
