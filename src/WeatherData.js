4d7c95d69e870585cd63e4a2b3058127e3c4daf6
psychokot11 <z.stawarska@gmail.com>
2021-12-23 15:30:59 +0100

    Styled WeatherData component

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -5,13 +5,6 @@ export default function WeatherData(props) {
   return (
     <div className="row">
       <div className="col-md-6 left-column">
-        <p>
-          <img
-            src={require("./images/dummy.png")}
-            alt="weather icon"
-            className="icon"
-          ></img>
-        </p>
         <div>
           <h1 className="city">{props.data.name}</h1>
           <CurrentDate />
@@ -19,6 +12,13 @@ export default function WeatherData(props) {
             <h3 className="humidity">Humidity: {props.data.humidity}%</h3>
             <h3 className="wind">Wind: {props.data.wind}km/h</h3>
             <h3 className="description">{props.data.description}</h3>
+            <p>
+              <img
+                src={props.data.icon}
+                alt="weather icon"
+                className="icon"
+              ></img>
+            </p>
           </div>
         </div>
       </div>
